d9078db39d5cfd5fa8532d943de9c74dd4d99f66
Shane <shane.pepperell@me.com>
2024-02-18 17:56:36 +0000

    gitignore for config as it has secrets

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_NICK = "MurphyAI"  # Bot's Twitch username
 TWITCH_TOKEN = "oauth:vrqm74lwywtc7i0yuefw0039jtvktm"  # Bot's OAuth token, get from https://twitchapps.com/tmi/
 TWITCH_CLIENT_ID = "bhr9s5c1j8rfn0lyvb4qszrscecism"  # Add your Twitch client ID here
-TWITCH_INITIAL_CHANNELS = ["IHasPeks"]  # List of channels the bot will join
+TWITCH_INITIAL_CHANNELS = ["OfficiallySp"]  # List of channels the bot will join
 
 # AI Configuration
 OPENAI_API_KEY = "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"  # API Key for OpenAI, used for the AI command
